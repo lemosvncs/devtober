a6e0ca45daadafbd8ede7e6c87fa42ed00856bdb
Vini Lemos <lemos.vncs@gmail.com>
2020-10-16 23:59:15 -0300

    The game so far and re-organization

--- Untitled Card Game Interrogation/Source/Cards/CardBase.gd
+++ Untitled Card Game Interrogation/Source/Cards/CardBase.gd
@@ -1,14 +1,15 @@
 extends Control
+tool
 
-var cardname:= "moveR"
+var cardname:= "moveL"
 onready var cardInfo = CardsDatabase.DATA[CardsDatabase.get(cardname)]
 onready var cardImage = str("res://Assets/Cards/", cardInfo[0], "/", cardname, ".png")
 
 func _ready():
 	
-	$H2/V1.add_constant_override("separation", 15) # dá uma margem pros elementos
+	$H2/V1.add_constant_override("separation", 7) # dá uma margem pros elementos
 	$H2/V1/CardName.text = cardInfo[2]
-	$H2/V1/Image.texture = load(cardImage)
+	$Image.texture = load(cardImage)
 	$H2/V1/Description.text = cardInfo[3]
 	
 	print(cardInfo)
